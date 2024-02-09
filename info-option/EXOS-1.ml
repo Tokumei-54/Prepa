b9443b6a6801472e967e148c4e583cc6019b639f
Tokumei-54 <leonardwilsonb@gmail.com>
2024-02-09 18:56:07 +0100

    EXOS-1 Ex 1 Q a b c

---
+++ info-option/EXOS-1.ml
@@ -0,0 +1,13 @@
+(*Exercice 1.1*)
+
+(*a*) 
+let x = 2.
+let x_8 = let x_2 = x *. x in let x_4 = x_2 *. x_2 in x_4 *. x_4
+
+(*b*)
+let a_0 = let e_0 = 8.85418782e-12 and h_ = 1.054571818e-34 and m_e = 9.19e-31 and e = 1.602176634e-19 in (4. *. Float.pi *. e_0 *. h_ *. h_) /. (m_e *. e *. e)
+
+(*c*)
+let th x = let e_2x = exp (2. *. x) in (e_2x -. 1.) /. (e_2x +. 1.)
+let t = th 2.
+let argth x = let ln_x = log x in 1. +. ln_x
