c6fbd17d3d0f285a98066a26d931ec72bb3aefaf
Tokumei-54 <leonardwilsonb@gmail.com>
2024-03-01 14:29:00 +0100

    EXOS-3 Q3

--- info-option/EXOS-1.ml
+++ info-option/EXOS-1.ml
@@ -2,13 +2,25 @@
 
 (*a*) 
 let x = 2.
-let y = let x_2 = x *. x in let x_4 = x_2 *. x_2 in x_4 *. x_4
+let y = 
+  let x_2 = x *. x in 
+  let x_4 = x_2 *. x_2 in 
+  x_4 *. x_4
 
 (*b*)
-let a_0 = let e_0 = 8.85418782e-12 and h_ = 1.054571818e-34 and m_e = 9.19e-31 and e = 1.602176634e-19 in (4. *. Float.pi *. e_0 *. h_ *. h_) /. (m_e *. e *. e)
+let a_0 = 
+  let e_0 = 8.85418782e-12 and
+      h_ = 1.054571818e-34 and 
+      m_e = 9.19e-31 and 
+      e = 1.602176634e-19 
+  in 
+  4. *. Float.pi *. e_0 *. h_ *. h_ /. (m_e *. e *. e)
 
 (*c*)
-let th x = let e_2x = exp (2. *. x) in (e_2x -. 1.) /. (e_2x +. 1.)
+let th x = 
+  let e_2x = exp (2. *. x) in
+  (e_2x -. 1.) /. (e_2x +. 1.)
+
 let argth x = (log ((1. +. x) /. (1. -. x))) /. 2.
 
 (*Exercice 1.2*)
@@ -47,7 +59,7 @@ let volume n h b d =
     sb *. h
   )
 
-let volume8 h b d = volume 8. h b d
+let volume8 = volume 8.
 
 let () = Printf.printf "l'anneau à un volume de %f m^3 \n" (volume8 54. 54. 54.)
 
@@ -82,7 +94,7 @@ let tPasc2 l =
 let () = print_string "nombre de lignes : " ; tPasc (read_int ())
 
 (*Exercice 1.6*)
-(*a*) (*erreur virgule*)
+(*a*) (*&&*)
 (*b*) (*5*)
 (*c*) (*erreur and est silmultané donc x n'est pas definis*)
 (*d*) (*erreur*)
---
+++ info-option/EXOS-3.ml
@@ -0,0 +1,81 @@
+(*Exercice 3.1*)
+
+(*a*)
+let rec fact  = function
+  |0 -> 1
+  |x -> x * fact (pred x)
+
+
+let fact_t =
+  let rec ft acc  = function
+    | 0. -> acc
+    | x -> ft (x *. acc) (x -. 1.)
+  in ft 1. 
+
+(*b*)
+(*\sum_{0}^{\infty} \frac{1}{n!} = e *)
+let rec e = function
+  | 0. -> 1.
+  | n -> 1. /. fact_t n +. e (n -. 1.)
+
+let aprox_e = e 2000.
+
+(*c*)
+(*racine de 2 pi *)
+
+(*Exercice 3.2*)
+
+(*a*)
+let rec u = function
+  |0 -> 1.
+  |n -> sin (u (pred n))
+
+let u_30 = u 30
+
+let aprox_u x =
+  let rec a_u n v x = match v < x with
+  | true -> n
+  | false -> a_u (succ n) (sin v ) x
+  in
+  a_u 0 1.
+
+let n_a = aprox_u 1e-3 (*2999989*)
+
+(*Exercice 3.3*)
+
+(*a*) 
+
+let rec pgcd a b = match a mod b with
+  | 0 -> b
+  | r -> pgcd b r
+
+(*b*)
+
+let pgcdi a_ b_ = 
+  let a = ref a_ in
+  let b = ref b_ in
+  while !b <> 0 do 
+    let r = !a mod !b in
+    a := !b;
+    b := r;
+  done ;
+  !a
+
+(*c*)
+(*un O de ln(a) ou ln(b)*)
+
+(*Exercice 3.3*)
+
+(*a*) 
+let rec bin = function
+ |0 -> "0"
+ |n -> string_of_int (n mod 2) ^ bin (n / 2)  
+
+let t = bin 54
+
+(*b*)
+let rec int_of_bin = function
+  |"" -> 0
+  |ch -> int_of_bin String.sub ch 
+
+let u = ((((1*2 + 1)*2 + 0)*2 + 1 )*2 + 1) *2 +0
