b384f418116595f91763aab4fc9328de07798e7d
Tokumei-54 <leonardwilsonb@gmail.com>
2024-03-12 16:59:27 +0100

    EXOS-4

---
+++ info-option/EXOS-4.ml
@@ -0,0 +1,30 @@
+(*Exercice 4.1*)
+
+(*a*)
+let implication a b = b || not a  
+let implication2 a b = if a then true else b
+let g = implication false true
+
+(*b*)
+let xor a b = a||b || not a && b
+(*c*)
+let egalitéQ (a, b) (c,d) = a*d = b*c
+(*d*)
+let egalitéQ2 ((a, b),(c,d)) = a*d = b*c
+
+(*Exercice 4.2*)
+let rec f a x = match x >= float_of_int a with
+  | false -> 0
+  | true -> f a (x /. (float_of_int a)) + 1
+
+let ilog2 = f 2
+
+let v = 2048.
+let t = ilog2 v
+let u = (log v) /. log 2.
+
+(*Exercice 4.3*)
+(*a*)
+let rec puiss x = function
+  |0 -> 1
+  |n -> (puiss x (pred n)) * x 
--- info-tronc-comun/TD/TD22.py
+++ info-tronc-comun/TD/TD22.py
@@ -27,11 +27,14 @@ def convolution(im,b):
     c = np.zeros((x,y), dtype='uint8')
     n , _   = b.shape
     k = n//2
-    for i in range(1,x-1):
-        for j in range(1,y-1):
-            for i_1 in range(n):
-              for j_1 in range(n):
-                  c[i][j]+= a[i-k+i_1][j-k+j_1] * b[i_1][j_1]
+    for i in range(k,x-k):
+        for j in range(k,y-k):
+            for rgb in range(3):
+                pixel_sum = 0
+                for i_1 in range(n):
+                    for j_1 in range(n):
+                        pixel_sum += a[i - k + i_1][j - k + j_1][rgb] * b[i_1][j_1]
+                c[i][j][rgb] = np.clip(pixel_sum, 0, 255)
     return Image.fromarray(c)
                   
 
