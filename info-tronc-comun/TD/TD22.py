c0d547089d81c1a088e7b80646fdaeb5aed044da
Tokumei-54 <leonardwilsonb@gmail.com>
2024-03-13 20:47:26 +0100

    TD22

--- info-tronc-comun/TD/TD22.py
+++ info-tronc-comun/TD/TD22.py
@@ -24,7 +24,7 @@ def contours(im,s=20):
 def convolution(im,b):
     a = np.array(im)
     x , y , _ = a.shape
-    c = np.zeros((x,y), dtype='uint8')
+    c = np.zeros((x,y,3), dtype='uint8')
     n , _   = b.shape
     k = n//2
     for i in range(k,x-k):
@@ -34,14 +34,43 @@ def convolution(im,b):
                 for i_1 in range(n):
                     for j_1 in range(n):
                         pixel_sum += a[i - k + i_1][j - k + j_1][rgb] * b[i_1][j_1]
-                c[i][j][rgb] = np.clip(pixel_sum, 0, 255)
+                c[i][j][rgb] = max(0, min(pixel_sum, 255))
     return Image.fromarray(c)
-                  
+
+def netteté(im):
+    return convolution(im,np.array([[0,-1,0],[-1,5,-1],[0,-1,0]]))
+
+def niveau_gris(im):
+    tab = np.array(im)
+    a, b, c = tab. shape
+    cadre =  np.zeros((a,b), dtype='uint8')
+    for i in range(a):
+        for j in range(b):
+            cadre[i][j] = 0.299 * tab[i][j][0] + 0.587 * tab[i][j][1] + 0.114 * tab[i][j][2]
+    return Image.fromarray(cadre)
+
+def convolution2(im, b):
+    tab = np.array(niveau_gris(convolution(im,b)))
+    a, b = tab. shape
+    cadre =  np.zeros((a,b), dtype='uint8')
+    for i in range(a):
+        for j in range(b):
+            cadre[i][j] = 255 - tab[i][j]
+    return Image.fromarray(cadre)
+
+def contours2(im):
+    # b=np.array([[1,1,1],[1,-8,1],[1,1,1]])
+    b=np.array([[1,1,1,1,1],[1,1,1,1,1],[1,1,-24,1,1],[1,1,1,1,1],[1,1,1,1,1]])
+    return convolution2(im, b)
 
 
 def main():
-    contours(im,20).save("contours.png")
-    convolution(im,np.array([[1,1,1],[1,1,1],[1,1,1]])/9).save("convolution.png")
+    # contours(im,20).save("contours.png")
+    # b_3_9 = np.array([[1,1,1],[1,1,1],[1,1,1]])/9
+    # b_5_25 = np.array([[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]])/25
+    # convolution(im,b_5_25).save("convolution.png")
+    # netteté(im).save("netteté.png")
+    contours2(im).save("contour2.png")
     
 if __name__ == '__main__':
     main()
