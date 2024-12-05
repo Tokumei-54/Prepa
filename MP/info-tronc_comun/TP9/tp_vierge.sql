12ce9e21947f39e6c37cd3b3fa19c8c2529f5d08
Tokumei-54 <leonardwilsonb@gmail.com>
2024-12-05 10:28:16 +0100

    0

---
+++ MP/info-tronc_comun/TP9/tp_vierge.sql
@@ -0,0 +1,68 @@
+ -- Quelles sont toutes les informations sur les villes ?
+ SELECT * FROM villes;
+-- 11. Quelles sont toutes les informations sur les peintres ?
+
+-- 12. Quels sont les prénoms et noms des peintres ?
+
+-- 13. Quels sont, sans doublons, les noms des musées ?
+
+-- 14. Quels sont les peintres de prénoms Michelangelo ?
+
+-- 15. Quel est le nom commun des peintres qui en ont un ?
+
+-- 16. Quels sont prénoms et noms des peintres qui n'ont vécu qu'au XVIIIe siècle
+-- ou dont l'identifiant du pays est 1 (la France) en ayant vécu au moins 70 ans ?
+
+-- 17. Identifiant et le nom des musées, en renommant l'identifiant en id_musee 
+-- et le nom en nom_musee
+
+-- 18. Donner l'ensemble des noms qui sont nom de famille ou nom commun d'un peintre
+
+-- 19. Quelles sont les années qui ont vu la naissance et la mort d'un peintre ?
+
+-- 20. Quels sont les identifiants des pays pour lesquels aucun peintre originaire
+-- de ce pays n’est né après 1800.
+
+-- 21. Quelles sont les associations possibles entre un nom d'oeuvre et un nom de musée ?
+
+-- 22. Quels sont les prénoms et noms de peintres, ainsi que les noms des pays 
+-- dans lesquels un peintre a pu voyager, sachant que l'on ne prend pas 
+-- en compte un pays dont le peintre est originaire ?
+
+-- 23. Quels sont les noms des peintres avec le nom de leurs oeuvres ?
+-- Avec jointure
+
+-- Avec produit cartésien
+
+-- 24. Quelles sont les noms des oeuvres exposées au Musée du Louvre ?
+ 
+-- 25. Quels sont, sans doublons, les identifiants et noms des musées qui exposent
+-- au moins une oeuvre du peintre de nom commun Le Titien ?
+
+-- 26. Quels sont les noms des peintres et les noms des musées situés en France
+-- qui exposent au moins une de leurs oeuvres ?
+
+-- 27. Identifiants des peintres dont aucune œuvre n’est exposée
+
+-- 28. Noms, sans doublons, des pays dont au moins un peintre est originaire
+-- et qui contiennent au moins un musée
+
+ -- 29. Noms des pays qui contiennent un musée mais dont aucun peintre n’est originaire
+ 
+-- 30. Quels sont les identifiants, prénoms et noms des peintres 
+-- du même pays que le peintre connu sous le nom Sandro Botticelli ?
+-- Avec jointure
+
+-- 31. Donner les noms de tableaux qui sont ceux d'au moins deux oeuvres différentes
+
+-- 32. Requête qui aboutit sur une table d’attributs id, nom, où nom est le nom d’usage
+-- d’un peintre s’il en a un et le nom de famille sinon.
+
+-- 33. Donner, sans doublons, le nom des peintres qui sont exposés dans un musée
+-- dans lequel il y a au moins un autre peintre de même nationalité que ce peintre
+-- qui est exposé
+
+-- 34. Donner, sans doublons le numéro et le nom des peintres qui ont peint 
+-- au moins une oeuvre exposée dans un pays dans lequel est exposé dans une
+-- autre ville de ce pays au moins une oeuvre d'un peintre qui est né 
+-- ou mort strictement plus tôt.
